367c3f1d17a02ca00da2d214f66f536ae3e6c92c
Joel Christner <joel.christner@gmail.com>
2016-10-11 08:42:28 -0700

    NuGet v1.0.5, fix for SendRequestSafe

--- RestWrapper/RestRequest.cs
+++ RestWrapper/RestRequest.cs
@@ -412,205 +412,72 @@ namespace RestWrapper
            byte[] Data
            )
         {
-            #region Check-for-Null-Values
-
-            if (String.IsNullOrEmpty(URL)) throw new ArgumentNullException("URL");
-            if (String.IsNullOrEmpty(URL)) throw new ArgumentNullException("Method");
-
-            #endregion
-
-            #region Check-Method
-
-            if ((String.Compare(Method.ToLower().Trim(), "head") != 0) &&
-                (String.Compare(Method.ToLower().Trim(), "get") != 0) &&
-                (String.Compare(Method.ToLower().Trim(), "post") != 0) &&
-                (String.Compare(Method.ToLower().Trim(), "put") != 0) &&
-                (String.Compare(Method.ToLower().Trim(), "delete") != 0))
+            try
             {
-                throw new ArgumentOutOfRangeException("Method");
+                return SendRequestInternal(URL, ContentType, Method, User, Password, EncodeCredentials, IgnoreCertErrors, CertificateFile, CertificatePassword, UserHeaders, Data);
             }
-
-            #endregion
-
-            #region Setup-Webrequest
-
-            if (IgnoreCertErrors)
+            catch (WebException we)
             {
-                ServicePointManager.ServerCertificateValidationCallback = Validator;
-            }
+                #region WebException
 
-            HttpWebRequest client = (HttpWebRequest)WebRequest.Create(URL);
-            client.KeepAlive = false;
-            client.Method = Method.ToUpper().Trim();
-            client.AllowAutoRedirect = true;
-            client.Timeout = 30000;
-            client.ContentLength = 0;
-            client.ContentType = ContentType;
-            client.UserAgent = "RestWrapper (www.github.com/jchristn/RestWrapper)";
+                RestResponse resp = new RestResponse();
+                resp.Headers = null;
+                resp.ContentEncoding = null;
+                resp.ContentType = null;
+                resp.ContentLength = 0;
+                resp.ResponseURI = null;
+                resp.StatusCode = 0;
+                resp.StatusDescription = null;
+                resp.Data = null;
 
-            #endregion
-
-            #region Add-Certificate
-
-            if (!String.IsNullOrEmpty(CertificateFile))
-            {
-                if (!String.IsNullOrEmpty(CertificatePassword))
+                HttpWebResponse exceptionResponse = we.Response as HttpWebResponse;
+                if (exceptionResponse != null)
                 {
-                    X509Certificate2 cert = new X509Certificate2(@CertificateFile, CertificatePassword);
-                    client.ClientCertificates.Add(cert);
-                }
-                else
-                {
-                    X509Certificate2 cert = new X509Certificate2(CertificateFile);
-                    client.ClientCertificates.Add(cert);
-                }
-            }
+                    resp.ContentEncoding = exceptionResponse.ContentEncoding;
+                    resp.ContentType = exceptionResponse.ContentType;
+                    resp.ContentLength = exceptionResponse.ContentLength;
+                    resp.ResponseURI = exceptionResponse.ResponseUri.ToString();
+                    resp.StatusCode = (int)exceptionResponse.StatusCode;
+                    resp.StatusDescription = exceptionResponse.StatusDescription;
 
-            #endregion
-
-            #region Add-Headers
-
-            if (UserHeaders != null && UserHeaders.Count > 0)
-            {
-                foreach (KeyValuePair<string, string> pair in UserHeaders)
-                {
-                    if (String.IsNullOrEmpty(pair.Key)) continue;
-                    if (String.IsNullOrEmpty(pair.Value)) continue;
-
-                    if (String.Compare(pair.Key.ToLower().Trim(), "accept") == 0)
+                    if (exceptionResponse.Headers != null && exceptionResponse.Headers.Count > 0)
                     {
-                        client.Accept = pair.Value;
-                        continue;
-                    }
-
-                    if (String.Compare(pair.Key.ToLower().Trim(), "content-type") == 0)
-                    {
-                        client.ContentType = pair.Value;
-                        continue;
-                    }
-
-                    if (String.Compare(pair.Key.ToLower().Trim(), "host") == 0)
-                    {
-                        client.Host = pair.Value;
-                    }
-
-                    client.Headers.Add(pair.Key, pair.Value);
-                }
-            }
-
-            #endregion
-
-            #region Add-Auth-Info
-
-            if (!String.IsNullOrEmpty(User))
-            {
-                if (EncodeCredentials)
-                {
-                    string authInfo = User + ":" + Password;
-                    authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
-                    client.Headers.Add("Authorization", "Basic " + authInfo);
-                }
-                else
-                {
-                    client.Headers.Add("Authorization", User);
-                }
-            }
-
-            #endregion
-
-            #region Package-Payload
-
-            if ((String.Compare(Method.ToLower().Trim(), "post") == 0) ||
-                (String.Compare(Method.ToLower().Trim(), "put") == 0) ||
-                (String.Compare(Method.ToLower().Trim(), "delete") == 0))
-            {
-                if (Data != null && Data.Length > 0)
-                {
-                    client.ContentLength = Data.Length;
-                    client.ContentType = ContentType;
-                    Stream clientStream = client.GetRequestStream();
-                    clientStream.Write(Data, 0, Data.Length);
-                    clientStream.Close();
-                }
-            }
-
-            #endregion
-
-            #region Submit-Request-and-Build-Response
-
-            HttpWebResponse response = (HttpWebResponse)client.GetResponse();
-            RestResponse ret = new RestResponse();
-            ret.ContentEncoding = response.ContentEncoding;
-            ret.ContentType = response.ContentType;
-            ret.ContentLength = response.ContentLength;
-            ret.ResponseURI = response.ResponseUri.ToString();
-            ret.StatusCode = (int)response.StatusCode;
-            ret.StatusDescription = response.StatusDescription;
-
-            #endregion
-
-            #region Headers
-
-            if (response.Headers != null)
-            {
-                if (response.Headers.Count > 0)
-                {
-                    ret.Headers = new Dictionary<string, string>();
-
-                    for (int i = 0; i < response.Headers.Count; i++)
-                    {
-                        string key = response.Headers.GetKey(i);
-                        string val = "";
-                        int valCount = 0;
-                        foreach (string value in response.Headers.GetValues(i))
+                        resp.Headers = new Dictionary<string, string>();
+                        for (int i = 0; i < exceptionResponse.Headers.Count; i++)
                         {
-                            if (valCount == 0)
+                            string key = exceptionResponse.Headers.GetKey(i);
+                            string val = "";
+                            int valCount = 0;
+
+                            foreach (string value in exceptionResponse.Headers.GetValues(i))
                             {
-                                val += value;
-                                valCount++;
+                                if (valCount == 0)
+                                {
+                                    val += value;
+                                    valCount++;
+                                }
+                                else
+                                {
+                                    val += "," + value;
+                                    valCount++;
+                                }
                             }
-                            else
-                            {
-                                val += "," + value;
-                                valCount++;
-                            }
+
+                            resp.Headers.Add(key, val);
                         }
-                        ret.Headers.Add(key, val);
                     }
+
+                    resp.Data = StreamToBytes(exceptionResponse.GetResponseStream());
                 }
+
+                return resp;
+
+                #endregion
             }
-
-            #endregion
-
-            #region Payload
-
-            StringBuilder sb = new StringBuilder();
-            Byte[] buf = new byte[8192];
-            Stream responseStream = response.GetResponseStream();
-
-            if (responseStream != null)
+            catch (Exception e)
             {
-                if (responseStream.CanRead)
-                {
-                    ret.Data = StreamToBytes(responseStream);
-                    ret.ContentLength = ret.Data.Length;
-                    responseStream.Close();
-                }
-                else
-                {
-                    ret.Data = null;
-                    ret.ContentLength = 0;
-                }
+                throw e;
             }
-            else
-            {
-                ret.Data = null;
-                ret.ContentLength = 0;
-            }
-
-            #endregion
-
-            return ret;
         }
 
         #endregion
